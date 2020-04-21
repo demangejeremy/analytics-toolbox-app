b5da4a56aa3b228d1d09dd2fa6c08811c02f2021
Jérémy DEMANGE <j@lachainemedias.com>
2020-04-21 02:43:56 +0200

    Mise en place des analyses

---
+++ api/liste_analyses.py
@@ -0,0 +1,58 @@
+#
+# Connexion à l'application
+#
+
+# Importations
+import pymongo
+from os import environ 
+from flask import Flask
+from flask import request
+from flask import escape
+from flask import jsonify
+from datetime import datetime  
+from datetime import timedelta  
+
+# Json webToken
+# import jwt
+# import simplejson as json
+
+# Mise en place de l'application
+app = Flask(__name__)
+
+# Retour message inscription
+@app.route("/api/liste_analyses", methods=["POST"])
+def idhn():
+    
+    # Recupérer données du formulaire
+    login = escape(request.form['user'])
+    dossier = escape(request.form['dossier'])
+
+    # Enregistrer en BDD
+    try:
+        myclient = pymongo.MongoClient(environ.get('MONGODB_URL'))
+    except:
+        print("Connexion impossible à la BDD")
+        print(environ.get('MONGODB_URL'))
+        raise
+
+    # Lien BDD
+    mydb = myclient["analyticstoolbox"]
+
+    # Ajouter dans une collection
+    mycol = mydb["analyses"]
+    myquery = { "utilisateur": login, "dossier": dossier }
+    mydoc = mycol.find(myquery)
+
+    # Stockage
+    idc = 0
+    content = []
+
+    # Affichage
+    print("Avant affichage")
+    for x in mydoc:
+        content.append({"id": idc, "lien": x["lien"], "nom": x["nom"], "description": x["description"]})
+        print(content)
+        idc += 1
+
+    # Retour de résultat
+    return jsonify(success="yes", content=content)
